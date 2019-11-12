fe1e7ebf5abef032aaca5f284bf8ddb7c3a64ff4
Shreshtha Pankaj <shreshtha.pankaj@colorado.edu>
2019-11-11 17:01:01 -0700

    socker listener for a-frame

---
+++ src/a-frame/AFrame-Listener.js
@@ -0,0 +1,58 @@
+import { type } from "os";
+
+const socket = null;
+var socketClosed = true;
+
+const startListening = (callBackOnMessage = null) => {
+  if (socket == null || socket == undefined || socketClosed) {
+    socket = new WebSocket("ws://localhost:44523");
+  }
+  // Connection opened
+  socket.addEventListener("open", function(event) {
+    // socket.send('Hello Server!');
+    socketClosed = false;
+    console.log("connection was open... ", event.data);
+  });
+  // Listen for messages
+  socket.addEventListener("message", function(event) {
+    console.log(
+      "Message from server ",
+      event.data,
+      " type of message: ",
+      typeof message
+    );
+    if (callBackOnMessage) {
+      callBackOnMessage(message);
+    }
+  });
+
+  socket.addEventListener("close", event => {
+    console.log("The connection has been closed.");
+    socketClosed = true;
+  });
+};
+
+const stopListening = () => {
+  if (socket) {
+    try {
+      socket.close();
+    } catch (err) {
+      console.log("WS - Socket closed.");
+    }
+  }
+};
+
+const wsMessageParser = message => {
+  if (message) {
+    try {
+      if (message["6deuler"] || message["wemos"]) {
+        return message;
+      } else {
+        console.log("don't know the message type.");
+        return null;
+      }
+    } catch (err) {
+      console.log("wsMessageParser. Error in reading message. ");
+    }
+  }
+};
